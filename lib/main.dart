4018d396adb38745c9fb7edf84e67f2e22edb1d4
AhmedMahdi0 <101126390+AhmedMahdi0@users.noreply.github.com>
2022-11-17 17:44:47 +0200

    fix some problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:whatsapp_clone_agora_firebase/firebase_options.dart';
 import 'package:whatsapp_clone_agora_firebase/shared/styles/themes.dart';
 import 'Layout/Cubit/CubitBloc.dart';
 import 'Layout/Cubit/StateBloc.dart';
